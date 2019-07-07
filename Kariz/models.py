5c2b51731f7f4623d6b8d126a49aa0d108e1e618
mostafa jalali <mostafa.jalali.1054@gmail.com>
2019-07-07 18:12:48 +0430

    phase5 git migtare

---
+++ Kariz/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+
+class Project(models.Model):
+    category = models.CharField(max_length=150)
+    name = models.CharField(max_length=200)
+    skills = models.CharField(max_length=1000,blank=True)
+    projectexplained = models.CharField(max_length=2000,blank=True)
+    cost = models.IntegerField()
+    fori = models.BooleanField()
+    motamayez = models.BooleanField()
+    creatoruname = models.ForeignKey(User,on_delete=models.CASCADE ,null=True,blank=True)
+
+    def __str__(self):
+        return self.name
+
