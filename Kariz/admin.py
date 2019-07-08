c36fd41d9d4424911e7fa6158e622141412acf5d
mostafa jalali <mostafa.jalali.1054@gmail.com>
2019-07-08 23:05:33 +0430

    add Free lancer model

--- Kariz/admin.py
+++ Kariz/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Project, Employer
+from .models import Project, Employer, FreeLancer
 
 # Register your models here.
 
 admin.site.register(Project)
 admin.site.register(Employer)
+admin.site.register(FreeLancer)
 
