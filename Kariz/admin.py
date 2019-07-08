83589fb10044b8211d21bead94c8148066c1e814
Hossein <ho3in14741@gmail.com>
2019-07-08 22:08:19 +0430

    class teaching form5

--- Kariz/admin.py
+++ Kariz/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Project
+from .models import Project,FreeLancer
 
 # Register your models here.
 
 admin.site.register(Project)
+admin.site.register(FreeLancer)
 
 
